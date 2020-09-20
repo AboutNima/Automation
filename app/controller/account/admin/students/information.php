e9c6827cc85a5936c47a0dcf892d872e85bbab48
Nima Asaadi <ni4.asadi.b@gmail.com>
2020-09-20 15:34:23 +0430

    Update information.php

--- app/controller/account/admin/students/information.php
+++ app/controller/account/admin/students/information.php
@@ -1,9 +1,10 @@
 <?php
-$data=$db->orderBy('id','DESC')->
+$data=$db->where('id',$id)->
 objectBuilder()->getOne('Students',null,[
 	'id','name','surname','name(en) as enN','surname(en) as enS','nationalCode','birthCNumber','phoneNumber',
 	'homeNumber','birthDay','education','address','job','createdAt'
 ]);
+if(empty($data)) die(header('location:/404'));
 $urlCrt[2]=$data->name.' '.$data->surname;
 $script='/public/account/admin/students/information';
 require_once 'app/controller/motherPage/header.php';

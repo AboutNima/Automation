a7fa2af1e68dafe6f929d5020d950ab5e0e51dc1
Mr-Offline <arsalan13812002@gmail.com>
2020-09-30 11:49:32 +0330

    CMaterials

--- app/view/account/admin/consumingMaterials/list.php
+++ app/view/account/admin/consumingMaterials/list.php
@@ -34,32 +34,23 @@
 				if(!empty($data)):
 					$num=1;
 					foreach($data as $item):
+						$item->unit=strtr($item->unit,[
+							'0'=>'میلی گرم',
+							'1'=>'گرم',
+							'2'=>'کیلوگرم',
+							'3'=>'میلی متر',
+							'4'=>'سانتی متر',
+							'5'=>'متر',
+							'6'=>'میلی لیتر',
+							'7'=>'لیتر',
+							'8'=>'عدد',
+						])
 						?>
 						<tr>
 							<td><?php echo $num++ ?></td>
 							<td><?php echo $item->title; ?></td>
 							<td><?php echo $item->company; ?></td>
-							<td>
-								<?php if($item->unit=='0'): ?>
-								<span class="label label-primary"> میلی گرم </span>
-								<?php elseif($item->unit=='1'): ?>
-								<span class="label label-primary"> گرم </span>
-								<?php elseif($item->unit=='2'): ?>
-								<span class="label label-primary"> کیلوگرم </span>
-								<?php elseif($item->unit=='3'): ?>
-								<span class="label label-primary"> میلی متر </span>
-								<?php elseif($item->unit=='4'): ?>
-								<span class="label label-primary"> سانتی متر </span>
-								<?php elseif($item->unit=='5'): ?>
-								<span class="label label-primary"> متر </span>
-								<?php elseif($item->unit=='6'): ?>
-								<span class="label label-primary"> میلی لیتر </span>
-								<?php elseif($item->unit=='7'): ?>
-								<span class="label label-primary"> لیتر </span>
-								<?php elseif($item->unit=='8'): ?>
-								<span class="label label-primary"> عدد </span>
-                                <?php endif; ?>
-							</td>
+							<td><span class="label label-primary"><?php echo $item->unit; ?></span></td>
 							<td><?php echo $item->count; ?></td>
 							<td>
 								<?php if(empty($item->description)): ?>
@@ -73,10 +64,9 @@
 										<i class="fal fa-ellipsis-h"></i>
 									</div>
 									<div class="menu">
-										<a href="/account/students/<?php echo $item->id ?>/information"><span><i class="far fa-file-alt"></i> دیگر مشخصات </span></a>
 										<a href="/account/students/<?php echo $item->id ?>/history"><span><i class="fas fa-history"></i> تاریخچه مواد مصرفی </span></a>
 										<a href="#edit" data-id="<?php echo $item->id ?>"><span><i class="far fa-file-edit"></i> ویرایش </span></a>
-										<a href="#edit" data-id="<?php echo $item->id ?>"><span><i class="far fa-trash"></i> حذف </span></a>
+										<a href="#delete" data-id="<?php echo $item->id ?>"><span><i class="far fa-trash"></i> حذف </span></a>
 									</div>
 								</div>
 							</td>
@@ -96,7 +86,7 @@
 		</div>
 	</div>
 </div>
-<div class="popup" popup-size="md" popup-title="ثبت مواد مصرفی جدید" id="add">
+<div class="popup" popup-size="sm" popup-title="ثبت مواد مصرفی جدید" id="add">
 	<div class="validation-message no-margin top"></div>
 	<form action="/ajax/account/admin/consumingMaterials/add" class="ajax-handler" method="post">
 		<input type="text" name="Token" value="<?php echo $_SESSION['Token'] ?>" hidden>
@@ -145,90 +135,68 @@
 		</div>
 	</form>
 </div>
-<div class="popup" popup-size="md" popup-title="ویرایش کارآموز" id="edit">
+<div class="popup" popup-size="sm" popup-title="ویرایش مواد مصرفی" id="edit">
 	<div class="validation-message no-margin top"></div>
-	<form action="/ajax/account/admin/students/edit" class="ajax-handler" method="post">
+	<form action="/ajax/account/admin/consumingMaterials/edit" class="ajax-handler" method="post">
 		<input type="text" name="Token" value="<?php echo $_SESSION['Token'] ?>" hidden>
 		<div class="row">
-			<div class="col-sm-4">
-				<div class="input-mask required" mask-type mask-label="نام">
-					<input type="text" name="data[name]" placeholder="نام کارآموز را وارد کنید" autocomplete="off">
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="input-mask required" mask-type mask-label="نام خانوادگی">
-					<input type="text" name="data[surname]" placeholder="نام خانوادگی کارآموز را وارد کنید" autocomplete="off">
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="input-mask required" mask-type mask-label="نام (انگلیسی)">
-					<input type="text" name="data[name_en]" placeholder="نام انگلیسی کارآموز را وارد کنید" autocomplete="off">
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="input-mask required" mask-type mask-label="نام خانوادگی (انگلیسی)">
-					<input type="text" name="data[surname_en]" placeholder="نام خانوادگی انگلیسی کارآموز را وارد کنید" autocomplete="off">
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="input-mask required" mask-type mask-label="نام پدر">
-					<input type="text" name="data[fatherName]" placeholder="نام پدر کارآموز را وارد کنید" autocomplete="off">
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="input-mask required" mask-type mask-label="تاریخ تولد">
-					<input type="text" name="data[birthDay]" placeholder="کلیک کنید" autocomplete="off">
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="input-mask required" mask-type mask-label="کد ملی">
-					<input type="text" name="data[nationalCode]" placeholder="کد ملی کارآموز را وارد کنید" autocomplete="off">
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="input-mask required" mask-type mask-label="شماره شناسنامه">
-					<input type="text" name="data[birthCNumber]" placeholder="شماره شناسنامه کارآموز را وارد کنید" autocomplete="off">
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="input-mask required" mask-type mask-label="شماره تلفن همراه">
-					<input type="text" name="data[phoneNumber]" placeholder="شماره تلفن همراه کارآموز را وارد کنید" autocomplete="off">
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="input-mask" mask-type mask-label="شماره تلفن ثابت">
-					<input type="text" name="data[homeNumber]" placeholder="شماره تلفن ثابت کارآموز را وارد کنید" autocomplete="off">
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="input-mask required" mask-type="select:search" mask-label="میزان تحصیلات">
-					<select name="data[education]">
-						<option value="0">راهنمایی</option>
-						<option value="1">دیپلم</option>
-						<option value="2">فوق دیپلم</option>
-						<option value="3">لیسانس</option>
-						<option value="4">فوق لیسانس</option>
-						<option value="5">دکتری</option>
-						<option value="6">فوق دکتری</option>
-					</select>
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="input-mask" mask-type mask-label="شغل">
-					<input type="text" name="data[job]" placeholder="شغل کارآموز را وارد کنید" autocomplete="off">
-				</div>
-			</div>
-			<div class="col-12">
-				<div class="input-mask required" mask-type mask-label="آدرس">
-					<input type="text" name="data[address]" placeholder="آدرس کارآموز را وارد کنید" autocomplete="off">
-				</div>
-			</div>
+            <div class="col-sm-6">
+                <div class="input-mask required" mask-type mask-label="نام">
+                    <input type="text" name="data[title]" placeholder="نام مواد را وارد کنید" autocomplete="off">
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="input-mask" mask-type mask-label="شرکت سازنده">
+                    <input type="text" name="data[company]" placeholder="شرکت سازنده مواد را وارد کنید" autocomplete="off">
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="input-mask required" mask-type="select:search" mask-label="واحد شمارش">
+                    <select name="data[unit]">
+                        <option value="0">میلی گرم</option>
+                        <option value="1">گرم</option>
+                        <option value="2">کیلوگرم</option>
+                        <option value="3">میلی متر</option>
+                        <option value="4">سانتی متر</option>
+                        <option value="5">متر</option>
+                        <option value="6">میلی لیتر</option>
+                        <option value="7">لیتر</option>
+                        <option value="8">عدد</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="input-mask required" mask-type mask-label="تعداد">
+                    <input type="text" name="data[count]" placeholder="تعداد مواد را وارد کنید" autocomplete="off">
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="input-mask" mask-type mask-label="توضیحات">
+                    <textarea name="data[description]" rows="3"></textarea>
+                </div>
+            </div>
 		</div>
 		<div class="clearFix">
 			<div class="hr"></div>
 			<div class="input-mask no-mask-margin">
-				<button class="btn btn-purple float-left"> ویرایش کارآموز </button>
+				<button class="btn btn-purple float-left"> ویرایش مواد </button>
 			</div>
 		</div>
 	</form>
+</div>
+<div class="popup" popup-size="sm" popup-title="حذف مواد مصرفی" id="delete">
+    <div class="validation-message no-margin top"></div>
+    <form action="/ajax/account/admin/consumingMaterials/delete" class="ajax-handler" method="post">
+        <input type="text" name="Token" value="<?php echo $_SESSION['Token'] ?>" hidden>
+        <input id="del" type="text" name="id" value="" hidden>
+        <div class="row">
+            <h6>آیا از حذف این مورد اطمینان دارید؟</h6>
+        </div>
+        <div class="clearFix">
+            <div class="hr"></div>
+            <div class="input-mask no-mask-margin">
+                <button class="btn btn-danger float-left"> حذف مواد </button>
+            </div>
+        </div>
+    </form>
 </div>
--- public/account/admin/consumingMaterials/list.js
+++ public/account/admin/consumingMaterials/list.js
@@ -14,28 +14,13 @@ $(document).ready(function()
     })
 
     $("a[href='#edit']").click(function(){
-        $.post('/ajax/account/admin/students/getData',{id:$(this).attr('data-id')},function(data){
-            data=$.parseJSON(data)[0]
-            changeSelect('#edit',$.parseJSON(data.education))
-            $("#edit input[name='data[name]']").val(data.name);
-            $("#edit input[name='data[surname]']").val(data.surname);
-            $("#edit input[name='data[name_en]']").val(data.name_en);
-            $("#edit input[name='data[surname_en]']").val(data.surname_en);
-            $("#edit input[name='data[fatherName]']").val(data.fatherName);
-            $("#edit input[name='data[nationalCode]']").val(data.nationalCode);
-            $("#edit input[name='data[birthCNumber]']").val(data.birthCNumber);
-            $("#edit input[name='data[phoneNumber]']").val(data.phoneNumber);
-            $("#edit input[name='data[homeNumber]']").val(data.homeNumber);
-            $("#edit input[name='data[birthDay]']").pDatepicker({
-                initialValue: true,
-                format: 'YYYY/MM/DD',
-                autoClose: true,
-                toolbox:{
-                    enabled: false
-                }
-            }).setDate(data.birthDay)
-            $("#edit input[name='data[address]']").val(data.address);
-            $("#edit input[name='data[job]']").val(data.job);
+        $.post('/ajax/account/admin/consumingMaterials/getData',{id:$(this).attr('data-id')},function(data){
+            data=$.parseJSON(data)[0];
+            changeSelect('#edit',$.parseJSON(data.unit))
+            $("#edit input[name='data[title]']").val(data.title)
+            $("#edit input[name='data[company]']").val(data.company)
+            $("#edit input[name='data[count]']").val(data.count)
+            $("#edit textarea").val(data.description);
             activePopup('#edit')
         })
     })
@@ -46,6 +31,21 @@ $(document).ready(function()
         {
             data=$.parseJSON(data)
             validationMessage(false,data.msg,data.type,data.err,'#edit .validation-message')
+            if(data.err==null) setTimeout(function(){location.reload()},1500)
+        })
+    })
+
+    $("a[href='#delete']").click(function(){
+        $('#del').attr('value', $(this).attr('data-id'))
+        activePopup('#delete')
+    })
+    $(document).on('submit','#delete form',function(e)
+    {
+        e.preventDefault();
+        ajaxHandler($(this)).done(function(data)
+        {
+            data=$.parseJSON(data)
+            validationMessage(false,data.msg,data.type,data.err,'#delete .validation-message')
             if(data.err==null) setTimeout(function(){location.reload()},1500)
         })
     })
